6f6a766b7ad533fae27926fc0f95376b274c5559
miketheologitis <miketh@outlook.com.gr>
2023-11-27 16:56:25 +0200

    per clients

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fdavg",
-    version="0.3",
+    version="0.4",
     packages=['fdavg', 'fdavg.metrics', 'fdavg.models', 'fdavg.strategies'],
     install_requires=[
         'tensorflow', 'numpy'
