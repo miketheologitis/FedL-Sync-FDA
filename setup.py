39fadb1265216d1285262e96ea8a4b8339cae4a5
miketheologitis <miketh@outlook.com.gr>
2023-11-29 12:00:36 +0200

    newsetup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fdavg",
-    version="0.4",
+    version="0.42",
     packages=['fdavg', 'fdavg.metrics', 'fdavg.models', 'fdavg.strategies'],
     install_requires=[
         'tensorflow', 'numpy'
