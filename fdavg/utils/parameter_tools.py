6f3acb82ab11a6192762c9e367b7bd1ec376e77c
miketheologitis <miketh@outlook.com.gr>
2023-12-08 20:22:46 +0200

    ongoing for densenet

--- fdavg/utils/parameter_tools.py
+++ fdavg/utils/parameter_tools.py
@@ -1,4 +1,5 @@
 from fdavg.data.mnist import MNIST_N_TRAIN, MNIST_CNN_BATCH_INPUT, MNIST_CNN_INPUT_RESHAPE, mnist_load_federated_data
+from fdavg.data.cifar10 import CIFAR10_N_TRAIN, CIFAR10_CNN_BATCH_INPUT, cifar10_load_federated_data
 from fdavg.models.lenet5 import get_compiled_and_built_lenet
 from fdavg.models.advanced_cnn import get_compiled_and_built_advanced_cnn
 
@@ -8,6 +9,7 @@ from functools import partial
 def derive_params(nn_name, ds_name, **kwargs):
     derived_params = {}
 
+    # Only for MNIST dataset
     if nn_name == 'AdvancedCNN':
         derived_params['compile_and_build_model_fn'] = partial(
             get_compiled_and_built_advanced_cnn, MNIST_CNN_BATCH_INPUT, MNIST_CNN_INPUT_RESHAPE, 10
@@ -18,8 +20,22 @@ def derive_params(nn_name, ds_name, **kwargs):
             get_compiled_and_built_lenet, MNIST_CNN_BATCH_INPUT, MNIST_CNN_INPUT_RESHAPE, 10
         )
 
+    # Only for CIFAR10 dataset
+    if nn_name == 'DenseNet121':
+        pass
+
+    if nn_name == 'DenseNet169':
+        pass
+
+    if nn_name == 'DenseNet201':
+        pass
+
     if ds_name == 'MNIST':
         derived_params['load_federated_data_fn'] = mnist_load_federated_data
         derived_params['n_train'] = MNIST_N_TRAIN
+
+    if ds_name == 'CIFAR10':
+        derived_params['load_federated_data_fn'] = cifar10_load_federated_data
+        derived_params['n_train'] = CIFAR10_N_TRAIN
 
     return derived_params
