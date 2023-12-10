af8dab077f80b283e063704f0e4eb0bd51210c84
miketheologitis <miketh@outlook.com.gr>
2023-12-10 19:32:20 +0200

    forgotten naive fed train_acc

--- fdavg/strategies/naive.py
+++ fdavg/strategies/naive.py
@@ -133,10 +133,15 @@ def naive_federated_simulation(test_dataset, federated_dataset, server_cnn, clie
                 
                 # ---------- Metrics ------------
                 acc = current_accuracy(client_cnns, test_dataset, compile_and_build_model_func)
-                epoch_metrics = EpochMetrics(epoch_count, total_rounds, total_fda_steps, acc)
+                train_acc = tf.reduce_mean([cnn.metrics[1].result() for cnn in client_cnns]).numpy()
+                epoch_metrics = EpochMetrics(epoch_count, total_rounds, total_fda_steps, acc, train_acc)
                 epoch_metrics_list.append(epoch_metrics)
                 print(epoch_metrics)  # remove
                 # -------------------------------
+
+                # Reset training accuracy
+                for cnn in client_cnns:
+                    cnn.metrics[1].reset_state()
                 
                 epoch_count += 1
                 
