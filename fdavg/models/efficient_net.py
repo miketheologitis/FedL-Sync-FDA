85bec97f0f4a3a21f571007027fe4ad16a63359a
miketheologitis <miketh@outlook.com.gr>
2024-05-17 17:08:13 +0300

    Transfer learning scenario, EfficientNet, CIFAR100

---
+++ fdavg/models/efficient_net.py
@@ -0,0 +1,95 @@
+from tensorflow.keras import layers, models
+import tensorflow as tf
+from tensorflow.keras.applications import EfficientNetB7, EfficientNetV2L
+
+
+class EfficientNet:
+    def __init__(self, name, input_shape=(32, 32, 3), classes=100, weight_file='XXX'):
+
+        base_model = None
+
+        if name == 'EfficientNetB7':
+            base_model = EfficientNetB7(include_top=False, weights='imagenet', input_shape=input_shape)
+        if name == 'EfficientNetV2L':
+            base_model = EfficientNetV2L(include_top=False, weights='imagenet', input_shape=input_shape)
+
+        x = base_model.output
+        x = layers.GlobalAveragePooling2D()(x)
+        x = layers.Dense(512, activation='relu')(x)
+        x = layers.Dropout(0.5)(x)
+        predictions = layers.Dense(classes, activation='softmax')(x)
+
+        self.model = models.Model(inputs=base_model.input, outputs=predictions)
+
+        self.model.load_weights(weight_file)
+
+    def __getattr__(self, name):
+        """
+        Automatically delegate method calls to the underlying Keras model.
+        This ensures that the custom class supports all methods of the
+        Keras model without having to define each one explicitly.
+        """
+        return getattr(self.model, name)
+
+    def step(self, batch):
+        x_batch, y_batch = batch
+        return self.train_on_batch(x=x_batch, y=y_batch)
+
+    def train(self, dataset):
+        for batch in dataset:
+            self.step(batch)
+
+    def set_trainable_variables(self, trainable_vars):
+        for model_var, var in zip(self.trainable_variables, trainable_vars):
+            model_var.assign(var)
+
+    def set_non_trainable_variables(self, non_trainable_vars):
+        """
+        Set the model's non-trainable variables.
+
+        Args:
+        - non_trainable_vars (list of tf.Tensor): A list of tensors representing the non-trainable variables to be set.
+
+        This method sets each of the model's trainable variables to the corresponding tensor in `non_trainable_vars`.
+        """
+        for model_var, var in zip(self.non_trainable_variables, non_trainable_vars):
+            model_var.assign(var)
+
+    @tf.function
+    def trainable_vars_as_vector(self):
+        return tf.concat([tf.reshape(var, [-1]) for var in self.trainable_variables], axis=0)
+
+    def per_layer_trainable_vars_as_vector(self):
+        layer_vectors = [
+            tf.concat([tf.reshape(var, [-1]) for var in layer.trainable_weights], axis=0)
+            for layer in self.layers
+            if layer.trainable_weights
+        ]
+
+        return layer_vectors
+
+    def set_layer_weights(self, layer_i, weights):
+        for model_var, var in zip(self.layers[layer_i].trainable_weights, weights):
+            model_var.assign(var)
+
+    def get_trainable_layers_indices(self):
+        trainable_layers_idx = [
+            i for i, layer in enumerate(self.layers)
+            if layer.trainable_weights
+        ]
+
+        return trainable_layers_idx
+
+
+def get_compiled_and_built_efficient_net(name, weight_file, cnn_batch_input, optimizer_fn):
+    efficient_net = EfficientNet(name=name, weight_file=weight_file)
+
+    efficient_net.compile(
+        optimizer=optimizer_fn(),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),  # we have softmax
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name='accuracy')]
+    )
+
+    efficient_net.build(cnn_batch_input)
+
+    return efficient_net
