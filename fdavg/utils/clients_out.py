e0926639fadd8dc21eec866ff28446487158b9be
miketheologitis <miketh@outlook.com.gr>
2024-09-15 10:21:24 +0300

    +1

--- fdavg/utils/clients_out.py
+++ fdavg/utils/clients_out.py
@@ -7,11 +7,11 @@ local_out_path = os.path.normpath(os.path.join(script_dir, f'{tmp_dir}/local_out
 
 def client_step_out_create(num_clients):
     for client_i in range(num_clients):
-        with open(f"{local_out_path}/client{client_i}.out", "w") as file:
+        with open(f"{local_out_path}/client{client_i+1}.out", "w") as file:
             pass
 
 def client_step_out(num_clients, step, sync):
     for client_i in range(num_clients):
-        with open(f"{local_out_path}/client{client_i}.out", "a") as file:
+        with open(f"{local_out_path}/client{client_i+1}.out", "a") as file:
             sync_str = "Synchronization STARTED!" if sync else "Synchronization avoided!"
             file.write(f"Step {step:5}: {sync_str}\n")
